6f1aa5a3f31221391de7fe9be57fde5c4814b3fe
Kaenoru <kaenoru@gmail.com>
2019-05-19 04:41:56 +0300

    finished

--- window-shopper/Views/CurrencyTextFiled.swift
+++ window-shopper/Views/CurrencyTextFiled.swift
@@ -11,6 +11,27 @@ import UIKit
 @IBDesignable
 class CurrencyTextFiled: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 32
+        let currencyLbl = UILabel(frame: CGRect(
+            x: (frame.size.height - size) / 2,
+            y: (frame.size.height / 2) - (size / 2),
+            width: size,
+            height: size ))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1585241866)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7174657534)
+        currencyLbl.layer.cornerRadius = 4.0
+        currencyLbl.clipsToBounds = true // required to always draw cornered radius
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current // default behavior
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+        
+    }
     
     override func prepareForInterfaceBuilder() {
         customizeInputField()
@@ -27,6 +48,7 @@ class CurrencyTextFiled: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 4.0
         textAlignment = .center
+        clipsToBounds = true // required to always draw cornered radius
         
         if let p = placeholder {
             let place = NSAttributedString(
